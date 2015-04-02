e55c1e7c1ee00cf844d3969440ec3c790ccae7a9
Tatsuya Kawano <tatsuya@hibaridb.org>
2015-04-03 07:23:45 +0800

    Add Dockerfiles using CentOS 7 base image.

---
+++ centos7/stop.sh
@@ -0,0 +1,35 @@
+#!/bin/sh
+
+##----------------------------------------------------------------------
+## Copyright (c) 2015 Hibari developers.  All rights reserved.
+##
+## Licensed under the Apache License, Version 2.0 (the "License");
+## you may not use this file except in compliance with the License.
+## You may obtain a copy of the License at
+##
+##     http://www.apache.org/licenses/LICENSE-2.0
+##
+## Unless required by applicable law or agreed to in writing, software
+## distributed under the License is distributed on an "AS IS" BASIS,
+## WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+## See the License for the specific language governing permissions and
+## limitations under the License.
+##
+## File     : brick.erl
+## Purpose  : brick top-level application startup
+##----------------------------------------------------------------------
+
+docker kill hibari6
+docker kill hibari5
+docker kill hibari4
+docker kill hibari3
+docker kill hibari2
+docker kill hibari1
+docker kill hibaribench
+docker rm hibari6
+docker rm hibari5
+docker rm hibari4
+docker rm hibari3
+docker rm hibari2
+docker rm hibari1
+docker rm hibaribench
